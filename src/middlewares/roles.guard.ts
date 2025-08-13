073a0b0a99e2dac785f7971c895f648caa029f79
Ameglebm <amegleeu@gmail.com>
2025-08-13 14:55:59 -0300

    feat - Ajuste ao roleGuards

--- src/middlewares/roles.guard.ts
+++ src/middlewares/roles.guard.ts
@@ -1,14 +1,22 @@
-import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Role } from '../models/common/enums/role.enum'; 
+import { Role } from '../models/common/enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {} // readonly pois não será reatribuído
 
   canActivate(context: ExecutionContext): boolean {
-    // Pega os roles definidos no decorator @Roles() do handler
-    const requiredRoles = this.reflector.get<Role[]>('roles', context.getHandler());
+    // Pega os roles definidos nos endpoints
+    const requiredRoles = this.reflector.get<Role[]>(
+      'roles',
+      context.getHandler(),
+    );
     if (!requiredRoles) return true; // se não houver roles, libera acesso
 
     const request = context.switchToHttp().getRequest();
